087b658cc8b80a46a03bfdb203d88e448b804b58
Kirtisagar Malshet <kirtisagar@gmail.com>
2014-12-18 15:57:20 +0530

    added scanner for iOS n WP

---
+++ Scanner/Scanner.WinPhone/Scanner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Scanner;
+using Scanner.WinPhone;
+using Xamarin.Forms;
+
+using ZXing.Mobile;
+using App = Scanner.WinPhone.App;
+
+[assembly: Xamarin.Forms.Dependency (typeof (Scanner.Wp.Scanner))]
+
+namespace Scanner.Wp
+{
+	public class Scanner: IScanner
+	{
+		#region IScanner implementation
+
+		async public Task<string> Scan ()
+		{
+			//NOTE: On Android you MUST pass a Context into the Constructor!
+			var scanner = new ZXing.Mobile.MobileBarcodeScanner(WinPhone.App.RootFrame.Dispatcher);
+			var result = await scanner.Scan();
+
+			//if (result != null)
+			//	Console.WriteLine("Scanned Barcode: " + result.Text);
+
+		    return result.Text;
+		}
+
+		#endregion
+
+
+	}
+}
+
---
+++ Scanner/Scanner.iOS/Scanner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Scanner.iOS;
+
+[assembly: Xamarin.Forms.Dependency (typeof (Scanner.iOS.Scanner))]
+
+namespace Scanner.iOS
+{
+	public class Scanner: IScanner
+	{
+		#region IScanner implementation
+
+		async public Task<string> Scan ()
+		{
+			//NOTE: On Android you MUST pass a Context into the Constructor!
+			var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+			var result = await scanner.Scan();
+
+			//if (result != null)
+				//Console.WriteLine("Scanned Barcode: " + result.Text);
+
+			return result.Text;
+		}
+
+		#endregion
+
+
+	}
+}
+
